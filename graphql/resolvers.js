dbe731c93d2f0ee18b02bc5388dfaa4ba8eac776
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2023-09-04 03:08:36 -0600

    fixes for the edit and update resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ const Guide = require("../models/guides.model");
 
 module.exports = {
   Query: {
-    async boat(_, {ID}) {
+    async boat(_, { ID }) {
       try {
         const boat = await Boat.findById(ID);
         return boat;
@@ -60,9 +60,9 @@ module.exports = {
         id: guide._id,
       };
     },
-    async deleteBoat(_, { ID }) {
+    async deleteBoat(_, ID) {
       try {
-        const wasDeleted = (await Boat.deleteOne(ID)).deletedCount;
+        const wasDeleted = (await Boat.deleteOne({ _id: ID.ID })).deletedCount;
         return wasDeleted > 0;
       } catch (err) {
         throw new Error(err);
@@ -77,14 +77,13 @@ module.exports = {
       }
     },
     async editBoat(_, { ID, swimlaneID }) {
-        try {
-            const result = await Boat.updateOne({ _id: ID }, { swimlaneID });
-            const wasUpdated = result.modifiedCount > 0;
-            return wasUpdated;
-        } catch (err) {
-            throw new Error(err);
-        }
-     
+      try {
+        const result = await Boat.updateOne({ _id: ID }, { swimlaneID });
+        const wasUpdated = result.modifiedCount > 0;
+        return wasUpdated;
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
